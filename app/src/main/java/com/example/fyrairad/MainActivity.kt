cb064befd478fb9eda957e42b1a7b79c890aa573
E30Pow3r <anan23ks@student.ju.se>
2024-11-13 19:53:55 +0100

    GUI Updated in PlayScreen

--- app/src/main/java/com/example/fyrairad/MainActivity.kt
+++ app/src/main/java/com/example/fyrairad/MainActivity.kt
@@ -2,17 +2,14 @@ package com.example.fyrairad
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.Animatable
-import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -20,29 +17,30 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import com.example.fyrairad.ui.theme.FyraIRadTheme
-import kotlinx.coroutines.launch
-import androidx.compose.animation.core.Animatable
-import androidx.compose.runtime.mutableStateOf
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.fyrairad.ui.theme.FyraIRadTheme
+import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
+
+var currPlayer: Int = 0
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,7 +50,7 @@ class MainActivity : ComponentActivity() {
             FyraIRadTheme {
                 val navController = rememberNavController()
 
-                NavHost(navController = navController, startDestination = "lobby") {
+                NavHost(navController = navController, startDestination = "spelplan") {
                     composable("spelplan") { PlayScreen(navController) }        // Säger: Dom här ska finnas i min router
                     composable("lobby") { Lobby(navController) }
                 }
@@ -66,65 +64,98 @@ class MainActivity : ComponentActivity() {
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun Lobby(navController: NavController) {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(50.dp)
-    ){
-        Box(Modifier
-            .align(Alignment.CenterHorizontally)
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(100.dp),
+            contentAlignment = Alignment.BottomCenter
         ) {
-            TextButton(onClick = { navController.navigate("spelplan") })
+            FilledTonalButton(onClick = { navController.navigate("spelplan") })
             {
                 Text("Spela 4 I Rad")
             }
-        }
+
     }
 }
 
 @Composable
 fun PlayScreen(navController: NavController) {
-    val showSlideImage = remember { mutableStateOf(false) }
+    val gridItems = remember { mutableStateListOf<Int>().apply { repeat(42) { add(R.drawable.empty) } } }
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
-            contentAlignment = Alignment.Center
+                .padding(top = 400.dp, bottom = 100.dp)
+                //.background(color = Color. Green) // Solid element background color
+            ,contentAlignment = Alignment.Center
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.spelfalt),
-                contentDescription = "FyraIRad Image",
-            )
-        }
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(7),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(0.dp)
+                    .padding(0.dp),
+                horizontalArrangement = Arrangement.spacedBy(-4.dp), // Adjust this value to change horizontal spacing
+                verticalArrangement = Arrangement.spacedBy(-0.2.dp) // Adjust this value to change vertical spacing
 
-        Row(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            for (i in 1..7) {       // Loopar programet så att man får 7 knappar
-                TextButton(
-                    onClick = { showSlideImage.value = true },
-                    Modifier
-                        .height(350.dp)
-                        .width(54.dp)
-                        .padding(0.dp)
-                ) {
-                    Text("$i")
+            ) {
+                items(gridItems.size) { index ->
+                    Box(
+                        modifier = Modifier
+                            .size(52.8.dp)
+                            .offset(x = -0.5.dp, y = 7.dp)
+                        ,contentAlignment = Alignment.Center
+                    ) {
+                        Image(
+                            painter = painterResource(id = gridItems[index]),
+                            contentDescription = "Grid Item"
+                            ,Modifier.size(45.dp)
+                        )
+                    }
                 }
             }
-            if (showSlideImage.value) {
-                SlideImage()
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopCenter
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.spelfalt),
+                    contentDescription = "FyraIRad Image",
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                for (i in 0 until 7) {
+                    TextButton(
+                        onClick = {
+                            for (j in 5 downTo 0) {
+                                if (gridItems[j * 7 + i] == R.drawable.empty) {
+                                    gridItems[j * 7 + i] = 
+                                    break
+                                }
+                            }
+                        },
+                        Modifier
+                            .height(350.dp)
+                            .width(54.dp)
+                    ) {
+                        Text("${i + 1}")
+                    }
+                }
             }
         }
     }
 }
 
 @Composable
-fun SlideImage() {
+fun SlideImage(X: Int, Y: Int) {
     val offsetY = remember { Animatable(450f) }
 
     LaunchedEffect(Unit) {
